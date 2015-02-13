fd4e6ecc6f0b0f56404d30252263636f9096f133
Frederic Devernay <frederic.devernay@inria.fr>
2015-02-13 22:45:50 +0100

    fix many bugs, see https://github.com/devernay/openfx-misc/commit/d0b5477be31394f6f9280d68e22164b7fdbdd4e2 closes #2

--- source/yadifdeint.cpp
+++ source/yadifdeint.cpp
@@ -55,153 +55,188 @@ private:
 
 
 
-// =========== GNU General Public License code start =================
+// =========== GNU Lesser General Public License code start =================
 
-//#define MIN(a,b) ((a) > (b) ? (b) : (a))
-//#define MAX(a,b) ((a) < (b) ? (b) : (a))
-//#define ABS(a) ((a) > 0 ? (a) : (-(a)))
-#define MIN(a,b) std::min(a,b)
-#define MAX(a,b) std::max(a,b)
-#define ABS(a) std::abs(a)
+// Yadif (yet another deinterlacing filter)
+// http://avisynth.org.ru/yadif/yadif.html
+// http://mplayerhq.hu
 
-#define MIN3(a,b,c) MIN(MIN(a,b),c)
-#define MAX3(a,b,c) MAX(MAX(a,b),c)
+// Original port to OFX/Vegas by George Yohng http://yohng.com
+// Rewritten after yadif relicensing to LGPL:
+// http://git.videolan.org/?p=ffmpeg.git;a=commit;h=194ef56ba7e659196fe554782d797b1b45c3915f
+
+
+// Copyright notice and licence from the original libavfilter/vf_yadif.c file:
+/*
+ * Copyright (C) 2006-2011 Michael Niedermayer <michaelni@gmx.at>
+ *               2010      James Darnley <james.darnley@gmail.com>
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+//#define FFMIN(a,b) ((a) > (b) ? (b) : (a))
+//#define FFMAX(a,b) ((a) < (b) ? (b) : (a))
+//#define FFABS(a) ((a) > 0 ? (a) : (-(a)))
+#define FFMIN(a,b) std::min(a,b)
+#define FFMAX(a,b) std::max(a,b)
+#define FFABS(a) std::abs(a)
+
+#define FFMIN3(a,b,c) FFMIN(FFMIN(a,b),c)
+#define FFMAX3(a,b,c) FFMAX(FFMAX(a,b),c)
 
 inline float halven(float f) { return f*0.5f; }
 inline int halven(int i) { return i>>1; }
 
 inline int one1(unsigned char *) { return 1; }
-inline float one1(float *) { return 1.0f/256.0f; }
-
-template<int ch,typename Comp,typename Diff>
-inline void filter_line_c(int mode, Comp *dst, const Comp *prev, const Comp *cur, const Comp *next, int w, long refs, int parity){
-    int x;
-    const Comp *prev2= parity ? prev : cur ;
-    const Comp *next2= parity ? cur  : next;
-
-    for(x=0; x<w; x++){
-        Diff c= cur[-refs];
-        Diff d= halven(prev2[0] + next2[0]);
-        Diff e= cur[+refs];
-        Diff temporal_diff0= ABS(prev2[0] - next2[0]);
-        Diff temporal_diff1=halven( ABS(prev[-refs] - c) + ABS(prev[+refs] - e) );
-        Diff temporal_diff2=halven( ABS(next[-refs] - c) + ABS(next[+refs] - e) );
-        Diff diff= MAX3(halven(temporal_diff0), temporal_diff1, temporal_diff2);
-        Diff spatial_pred= halven(c+e);
-        Diff spatial_score= ABS(cur[-refs-ch] - cur[+refs-ch]) + ABS(c-e)
-                          + ABS(cur[-refs+ch] - cur[+refs+ch]) - one1((Comp*)0);
+inline int one1(unsigned short *) { return 1; }
+inline float one1(float *) { return 0.f; }
 
 #define CHECK(j)\
-    {   Diff score= ABS(cur[-refs-ch+ j] - cur[+refs-ch- j])\
-                 + ABS(cur[-refs   + j] - cur[+refs   - j])\
-                 + ABS(cur[-refs+ch+ j] - cur[+refs+ch- j]);\
+    {   Diff score = FFABS(cur[mrefs + ch * (- 1 + (j))] - cur[prefs + ch * (- 1 - (j))])\
+                 + FFABS(cur[mrefs   + ch * (j)] - cur[prefs   - ch * (j)])\
+                 + FFABS(cur[mrefs + ch * (1 + (j))] - cur[prefs + ch * (1 - (j))]);\
         if(score < spatial_score){\
             spatial_score= score;\
-            spatial_pred= halven(cur[-refs  + j] + cur[+refs  - j]);\
+            spatial_pred= halven(cur[mrefs  + ch * (j)] + cur[prefs  - ch * (j)]);\
 
-        CHECK(-ch) CHECK(-(ch*2)) }} }}
-        CHECK( ch) CHECK( (ch*2)) }} }}
+/* The is_not_edge argument here controls when the code will enter a branch
+ * which reads up to and including x-3 and x+3. */
 
-        if(mode<2){
-            Diff b= halven(prev2[-2*refs] + next2[-2*refs]);
-            Diff f= halven(prev2[+2*refs] + next2[+2*refs]);
-#if 0
-            Diff a= cur[-3*refs];
-            Diff g= cur[+3*refs];
-            Diff max= MAX3(d-e, d-c, MIN3(MAX(b-c,f-e),MAX(b-c,b-a),MAX(f-g,f-e)) );
-            Diff min= MIN3(d-e, d-c, MAX3(MIN(b-c,f-e),MIN(b-c,b-a),MIN(f-g,f-e)) );
-#else
-            Diff max= MAX3(d-e, d-c, MIN(b-c, f-e));
-            Diff min= MIN3(d-e, d-c, MAX(b-c, f-e));
-#endif
-
-            diff= MAX3(diff, min, -max);
-        }
-
-        if(spatial_pred > d + diff)
-           spatial_pred = d + diff;
-        else if(spatial_pred < d - diff)
-           spatial_pred = d - diff;
-
-        dst[0] = (Comp)spatial_pred;
-
-        dst+=ch;
-        cur+=ch;
-        prev+=ch;
-        next+=ch;
-        prev2+=ch;
-        next2+=ch;
+#define FILTER(start, end, is_not_edge) \
+    for (x = start;  x < end; x++) { \
+        Diff c= cur[mrefs]; \
+        Diff d= halven(prev2[0] + next2[0]); \
+        Diff e= cur[prefs]; \
+        Diff temporal_diff0= FFABS(prev2[0] - next2[0]); \
+        Diff temporal_diff1=halven( FFABS(prev[mrefs] - c) + FFABS(prev[prefs] - e) ); \
+        Diff temporal_diff2=halven( FFABS(next[mrefs] - c) + FFABS(next[prefs] - e) ); \
+        Diff diff= FFMAX3(halven(temporal_diff0), temporal_diff1, temporal_diff2); \
+        Diff spatial_pred= halven(c+e); \
+ \
+        if (is_not_edge) {\
+            Diff spatial_score = FFABS(cur[mrefs-ch] - cur[prefs-ch]) + FFABS(c-e) \
+                              + FFABS(cur[mrefs+ch] - cur[prefs+ch]) - one1((Comp*)0); \
+            CHECK(-1) CHECK(-2) }} }} \
+            CHECK( 1) CHECK( 2) }} }} \
+        }\
+ \
+        if (!(mode&2)) { \
+            Diff b = halven(prev2[2 * mrefs] + next2[2 * mrefs]); \
+            Diff f = halven(prev2[2 * prefs] + next2[2 * prefs]); \
+            Diff max = FFMAX3(d - e, d - c, FFMIN(b - c, f - e)); \
+            Diff min = FFMIN3(d - e, d - c, FFMAX(b - c, f - e)); \
+ \
+            diff = FFMAX3(diff, min, -max); \
+        } \
+ \
+        if (spatial_pred > d + diff) \
+           spatial_pred = d + diff; \
+        else if (spatial_pred < d - diff) \
+           spatial_pred = d - diff; \
+ \
+        dst[0] = (Comp)spatial_pred; \
+ \
+        dst += ch; \
+        cur += ch; \
+        prev += ch; \
+        next += ch; \
+        prev2 += ch; \
+        next2 += ch; \
     }
-}
-
-inline void interpolate(unsigned char *dst, const unsigned char *cur0,  const unsigned char *cur2, int w)
-{
-    int x;
-    for (x=0; x<w; x++) {
-        dst[x] = (cur0[x] + cur2[x] + 1)>>1; // simple average
-    }
-}
-
-inline void interpolate(float *dst, const float *cur0,  const float *cur2, int w)
-{
-    int x;
-    for (x=0; x<w; x++) {
-        dst[x] = (cur0[x] + cur2[x] )*0.5f; // simple average
-    }
-}
-
 
 template<int ch,typename Comp,typename Diff>
-static void filter_plane(int mode, Comp *dst, long dst_stride, const Comp *prev0, const Comp *cur0, const Comp *next0, long refs, int w, int h, int parity, int tff)
+inline void filter_line_c(Comp *dst1,
+                          const Comp *prev1, const Comp *cur1, const Comp *next1,
+                          int w, int prefs, int mrefs, int parity, int mode)
 {
-	int y=0;
+    Comp *dst  = dst1;
+    const Comp *prev = prev1;
+    const Comp *cur  = cur1;
+    const Comp *next = next1;
+    int x;
+    const Comp *prev2 = parity ? prev : cur ;
+    const Comp *next2 = parity ? cur  : next;
 
-    if(((y ^ parity) & 1)) {
-        memcpy(dst, cur0 + refs, w*ch*sizeof(Comp));// duplicate 1
-    }else{
-        memcpy(dst, cur0, w*ch*sizeof(Comp));
-    }
+    /* The function is called with the pointers already pointing to data[3] and
+     * with 6 subtracted from the width.  This allows the FILTER macro to be
+     * called so that it processes all the pixels normally.  A constant value of
+     * true for is_not_edge lets the compiler ignore the if statement. */
+    FILTER(0, w, 1)
+}
 
-    y=1;
-    
-    if(((y ^ parity) & 1)) {
-        interpolate(dst + dst_stride, cur0, cur0 + refs*2, w*ch);   // interpolate 0 and 2
-    }else{
-        memcpy(dst + dst_stride, cur0 + refs, w*ch*sizeof(Comp)); // copy original
-    }
+#define MAX_ALIGN 8
+template<int ch,typename Comp,typename Diff>
+inline void filter_edges(Comp *dst1,
+                         const Comp *prev1, const Comp *cur1, const Comp *next1,
+                         int w, int prefs, int mrefs, int parity, int mode)
+{
+    Comp *dst  = dst1;
+    const Comp *prev = prev1;
+    const Comp *cur  = cur1;
+    const Comp *next = next1;
+    int x;
+    const Comp *prev2 = parity ? prev : cur ;
+    const Comp *next2 = parity ? cur  : next;
 
-    for(y=2; y<h-2; y++) {
-        if(((y ^ parity) & 1)) {
+    /* Only edge pixels need to be processed here.  A constant value of false
+     * for is_not_edge should let the compiler ignore the whole branch. */
+    FILTER(0, 3, 0)
+
+    dst  = dst1  + (w - 3)*ch;
+    prev = prev1 + (w - 3)*ch;
+    cur  = cur1  + (w - 3)*ch;
+    next = next1 + (w - 3)*ch;
+    prev2 = parity ? prev : cur;
+    next2 = parity ? cur  : next;
+
+    FILTER(w - 3, w, 0)
+}
+
+template<int ch,typename Comp,typename Diff>
+static void filter_plane(int mode, Comp *dst, int dst_stride,
+                         const Comp *prev0, const Comp *cur0, const Comp *next0,
+                         int refs, int w, int h, int parity, int tff)
+{
+    int pix_3 = 3 * ch;
+    for (int y = 0; y < h; ++y) {
+        if (((y ^ parity) & 1)) {
             const Comp *prev= prev0 + y*refs;
             const Comp *cur = cur0 + y*refs;
             const Comp *next= next0 + y*refs;
             Comp *dst2= dst + y*dst_stride;
+            int mode2 = y == 1 || y + 2 == h ? 2 : mode;
 
-            for(int k=0;k<ch;k++)
-                filter_line_c<ch,Comp,Diff>(mode, dst2+k, prev+k, cur+k, next+k, w, refs, (parity ^ tff));
-        }else{
-            memcpy(dst + y*dst_stride, cur0 + y*refs, w*ch*sizeof(Comp)); // copy original
+            for (int c = 0; c < ch; ++c) {
+                filter_line_c<ch,Comp,Diff>(dst2 + c + pix_3, prev + c + pix_3, cur + c + pix_3, next + c + pix_3, w - 6,
+                                            y + 1 < h ? refs : -refs,
+                                            y ? -refs : refs,
+                                            parity ^ tff, mode2);
+                filter_edges<ch,Comp,Diff>(dst2 + c, prev + c, cur + c, next + c, w,
+                                           y + 1 < h ? refs : -refs,
+                                           y ? -refs : refs,
+                                           parity ^ tff, mode2);
+            }
+        } else {
+            memcpy(&dst[y * dst_stride],
+                   &cur0[y * refs], w * ch * sizeof(Comp)); // copy original
         }
     }
-
-    y=h-2;
-
-    if(((y ^ parity) & 1)) {
-        interpolate(dst + (h-2)*dst_stride, cur0 + (h-3)*refs, cur0 + (h-1)*refs, w*ch);   // interpolate h-3 and h-1
-    }else{
-      memcpy(dst + (h-2)*dst_stride, cur0 + (h-2)*refs, w*ch*sizeof(Comp)); // copy original
-    }
-
-    y=h-1;
     
-    if(((y ^ parity) & 1)){
-        memcpy(dst + (h-1)*dst_stride, cur0 + (h-2)*refs, w*ch*sizeof(Comp)); // duplicate h-2
-    }else{
-        memcpy(dst + (h-1)*dst_stride, cur0 + (h-1)*refs, w*ch*sizeof(Comp)); // copy original
-    }
 }
 
-// =========== GNU General Public License code end =================
+// =========== GNU Lesser General Public License code end =================
 
 void YadifDeint::render(const OFX::RenderArguments &args)
 {
